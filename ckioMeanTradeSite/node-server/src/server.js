f9e20bfe08ff7099af5d714cf79a7f19b06bfcf0
KepsterUK <connorkennedy@me.com>
2020-05-08 14:35:52 +0100

    update

--- ckioMeanTradeSite/node-server/src/server.js
+++ ckioMeanTradeSite/node-server/src/server.js
@@ -4,7 +4,7 @@ const socketServer = require('./socketio');
 
 
 app.use((req, res, next) => {
-    res.setHeader("Access-Control-Allow-Origin", "http://localhost:4200");
+    res.setHeader("Access-Control-Allow-Origin", "http://localhost:8096");
     res.header('Access-Control-Allow-Credentials', true);
     res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE, OPTIONS");
