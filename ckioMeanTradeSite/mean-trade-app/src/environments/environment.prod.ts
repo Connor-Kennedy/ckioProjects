e78dd7bf3306cf4a55b9262d5b79390222353b3a
KepsterUK <connorkennedy@me.com>
2020-05-08 18:04:33 +0100

    more dev

--- ckioMeanTradeSite/mean-trade-app/src/environments/environment.prod.ts
+++ ckioMeanTradeSite/mean-trade-app/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  SOCKET_ENDPOINT: 'trade-socket.c-k.io',
-  API_ENDPOINT: 'trade-api.c-k.io'
+  SOCKET_ENDPOINT: 'http://trade-socket.c-k.io',
+  API_ENDPOINT: 'http://trade-api.c-k.io'
 };
