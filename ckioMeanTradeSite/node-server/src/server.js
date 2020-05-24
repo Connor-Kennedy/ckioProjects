db2b299daf23005a2a762ed55f0ced3f423c86a2
KepsterUK <connorkennedy@me.com>
2020-05-24 10:12:04 +0100

    update

--- ckioMeanTradeSite/node-server/src/server.js
+++ ckioMeanTradeSite/node-server/src/server.js
@@ -11,13 +11,15 @@ app.use((req, res, next) => {
     next();
   });
 
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "http://localhost:4200");
-  res.header('Access-Control-Allow-Credentials', true);
-  res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE, OPTIONS");
-  next();
-});
+
+// comment out for production! TODO: resolve with production env variable instead
+// app.use((req, res, next) => {
+//   res.setHeader("Access-Control-Allow-Origin", "http://localhost:4200");
+//   res.header('Access-Control-Allow-Credentials', true);
+//   res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//   res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE, OPTIONS");
+//   next();
+// });
   
 
 // app.get('/api/btcusd', (req, res, next) => {
